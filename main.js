1df30f27448fe485a43fd919eb912e47362d8d93
radek-czcz <radowiczek@gmail.com>
2021-09-27 19:44:45 +0200

    menubar unrolling with overflow settings accompl.

--- main.js
+++ main.js
@@ -1,13 +1,46 @@
-let toggleButton = toggleFunc();
+
+let widthChanger;
 function toggleFunc(){
+  let widthChanger = document.querySelector("div.layout");
   let menubar = document.getElementsByClassName('menubar')[0];
-  /*let menuCase = menubar.getAttribute("display");*/
-  /*let menuCase = menubar.style.display;*/
   let menuCase = document.querySelector(".menubar");
   if (getComputedStyle(menuCase).display === "none") {
-  menuCase.style.display = "block";
-} else if (getComputedStyle(menuCase).display === "block") {
+    menuCase.style.display = "block";
+    let widthChanger = document.querySelector("div.layout");
+    let widthChanger2 = getComputedStyle(widthChanger);
+    console.log(widthChanger.style.gridTemplateColumns);
+    for (nth = 0; nth < 10; nth++) {
+      doSetTimeout(nth*35, nth*30)
+      //setTimeout(menubarNarrower, 1000, nth*25);
+      //clearTimeout();
+      //menubarNarrower(nth*25);
+    }
 
-  menuCase.style.display = "none";
+  } else if (getComputedStyle(menuCase).display === "block") {
+
+    let widthChanger = document.querySelector("div.layout");
+    let widthChanger2 = getComputedStyle(widthChanger);
+    console.log(widthChanger.style.gridTemplateColumns);
+    for (nth2 = 0; nth2 < 10; nth2++) {
+      doSetTimeout(nth2*35, 300-nth2*30)
+      //setTimeout(menubarNarrower, 1000, 250-nth2*25);
+      //menubarNarrower(250-nth2*25);
+    }
+    menuCase.style.display = "none";
+
+
+
+    //menuCase.style.display = "none";
+  }
 }
+
+function menubarNarrower(iterStep) {
+  let widthChanger = document.querySelector("div.layout");
+  widthChanger.style.gridTemplateColumns = iterStep + "px 200px",100;
+  console.log(iterStep);
+  console.log(25*iterStep);
+}
+
+function doSetTimeout(int1, int2) {
+  setTimeout(menubarNarrower, int1, int2);
 }
