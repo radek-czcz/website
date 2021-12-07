24681761950b6282bf6727fbdfe28383adbc6a97
radek-czcz <radowiczek@gmail.com>
2021-12-07 11:14:57 +0100

    2021-12-07:

--- main.js
+++ main.js
@@ -4,35 +4,25 @@ var slider = document.getElementById("slid");
 
 function toggleFunc(){
   let widthChanger = document.querySelector("div.layout");
+  let widthChanger2 = getComputedStyle(widthChanger);
   let menubar = document.getElementsByClassName('menubar')[0];
   let menuCase = document.querySelector(".menubar");
   if (getComputedStyle(menuCase).display === "none") {
     menuCase.style.display = "block";
-    let widthChanger = document.querySelector("div.layout");
-    let widthChanger2 = getComputedStyle(widthChanger);
     console.log(widthChanger.style.gridTemplateColumns);
     for (nth = 0; nth < 10; nth++) {
       doSetTimeout(nth*35, nth*30)
-      //setTimeout(menubarNarrower, 1000, nth*25);
-      //clearTimeout();
-      //menubarNarrower(nth*25);
     }
 
   } else if (getComputedStyle(menuCase).display === "block") {
-
-    let widthChanger = document.querySelector("div.layout");
-    let widthChanger2 = getComputedStyle(widthChanger);
-    console.log(widthChanger.style.gridTemplateColumns);
-    for (nth2 = 0; nth2 < 10; nth2++) {
-      doSetTimeout(nth2*35, 300-nth2*30)
-      //setTimeout(menubarNarrower, 1000, 250-nth2*25);
-      //menubarNarrower(250-nth2*25);
-    }
-    menuCase.style.display = "none";
-
-
-
-    //menuCase.style.display = "none";
+      console.log(widthChanger.style.gridTemplateColumns);
+      for (nth2 = 0; nth2 < 10; nth2++) {
+        doSetTimeout(nth2*35, 300-nth2*30)
+      }
+      setTimeout(function(){
+        menuCase.style.display = "none";
+      }, 350)
+      //menuCase.style.display = "none";
   }
 }
 
@@ -50,4 +40,23 @@ function doSetTimeout(int1, int2) {
 slider.oninput = function() {
   var getDiv = document.getElementsByClassName("flexDiv")[0];
   getDiv.style.width = this.value;
+}
+
+function toggleFunc2(){
+  let widthChanger = document.querySelector("div.layout");
+  let widthChanger2 = getComputedStyle(widthChanger);
+  let menubar = document.getElementsByClassName('menubar')[0];
+  let menuCase = document.querySelector(".menubar");
+  if (getComputedStyle(menuCase).display === "none") {
+    menuCase.style.display = "block";
+    console.log(widthChanger.style.gridTemplateColumns);
+    doSetTimeout(5000, 20 );
+    doSetTimeout(100, 500 );
+
+  } else if (getComputedStyle(menuCase).display === "block") {
+      console.log(widthChanger.style.gridTemplateColumns);
+      doSetTimeout(5000, 20 );
+      doSetTimeout(100, 500 );
+      menuCase.style.display = "none";
+  }
 }
